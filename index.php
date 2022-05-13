277823b2f7b55c35a4146aff3ed033103ea5759d
Lara Filippone <lara.filippone@gmail.com>
2022-05-13 15:38:22 +0200

    defined istances of class Movie

--- index.php
+++ index.php
@@ -1,20 +1,25 @@
 <?php
-/*
-create un file index.php in cui:
-- è definita una classe ‘Movie’
-   => all'interno della classe sono dichiarate delle variabili d'istanza
-   => all'interno della classe è definito un costruttore
-   => all'interno della classe è definito almeno un metodo
-- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
+    /*
+    create un file index.php in cui:
+    - è definita una classe ‘Movie’
+    => all'interno della classe sono dichiarate delle variabili d'istanza
+    => all'interno della classe è definito un costruttore
+    => all'interno della classe è definito almeno un metodo
+    - vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
 
-*/
+    */
 
-//phpinfo();
+    //phpinfo();
 
-// definisco class Movie
-class Movie{
+    // definisco class Movie e seue istanze
+    class Movie{
+        public $title;
+        public $country;
+        public $release_date;
+        public $production;
+        public $running_time;
+    }
+
     
-}
-
 
 ?>
